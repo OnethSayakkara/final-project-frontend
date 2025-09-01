8514c202b48a92ce7e2d3f7dcf9e9c347299c9c8
oneth sayakkara <onethsayakkara@gmail.com>
2025-09-01 15:48:50 +0530

    setup project template and add tailwind css

---
+++ src/customer/home page/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div className='bg-red-500 text-orange-500'>
+     <p className='text-5xl'>home page</p>
+    </div>
+  )
+}
+
+export default HomePage
